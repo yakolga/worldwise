0c475fe8642a448e3915ac88cb5afd06d66ccd43
Olga Kulczycka <olga@webjaksklep.eu>
2025-11-07 23:53:34 +0100

    app structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ESLint } from 'eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), new ESLint()],
 })
