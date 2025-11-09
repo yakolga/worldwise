a56b89b92578fb86edab76c97182b2087fb04105
Olga Kulczycka <olga@webjaksklep.eu>
2025-11-09 23:57:10 +0100

    country list & cities list

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,11 @@
 import Spinner from '../components/Spinner';
+import Message from '../components/Message';
 import CityItem from './CityItem';
 import styles from './CityList.module.css';
 
 function CityList({cities, loading}) {
     if (loading) return <Spinner/>
+    if (!cities.length) return <Message message="Add your first city by clicking on a city on the map"/>
     return (
         <ul className={styles.cityList}>
             {cities.map(city => (
---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,29 @@
+import Spinner from '../components/Spinner';
+import Message from '../components/Message';
+import styles from './CountriesList.module.css';
+import CountryItem from './CountryItem';
+
+function CountriesList({cities, loading}) {
+    if (loading) return <Spinner/>
+    if (!cities.length) return <Message message="Add your first city by clicking on a city on the map"/>
+
+    const countries = [];
+    const result = [];
+
+    for (let i = 0; i < cities.length; i++) {
+        if (!countries.includes(cities[i].country)) {
+            countries.push(cities[i].country);
+            result.push(cities[i]);
+        }
+    }
+
+    return (
+        <ul className={styles.countryList}>
+            {result.map(country => (
+                <CountryItem country={country} key={country.id}/>
+            ))}
+        </ul>
+    )
+}
+
+export default CountriesList
