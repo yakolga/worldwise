617d41d9de10297d5e341cb0948b9986f279f46f
Olga Kulczycka <olga@webjaksklep.eu>
2025-11-29 17:17:02 +0100

    Updates

---
+++ assets/Logo-5b7f82c4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-d79fb059.js";const s="_logo_7b2ls_1",l={logo:s};function g(){return o.jsx("img",{src:"logo.png",alt:"WorldWise logo",className:l.logo})}export{g as L};
---
+++ assets/Product.module-3a9a795a.js
@@ -0,0 +1,1 @@
+const _="_product_1fxqe_1",t="_product__image_1fxqe_17",o={product:_,product__image:t};export{o as s};
